c3c7ffbace9b7f6700b0e55d8a33844607372dae
P Kul <1.0@kul.asia>
2018-02-16 12:22:17 -0600

    Fix error Devise could not find the `Warden::Proxy` instance on your request environment.

--- lib/generators/turbolog/templates/spec/rails_helper.rb
+++ lib/generators/turbolog/templates/spec/rails_helper.rb
@@ -13,7 +13,7 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   # Setup capybara
-  #config.include Warden::Test::Helpers
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Capybara::DSL
   # config.after :each do
   # 	Warden.test_reset!
